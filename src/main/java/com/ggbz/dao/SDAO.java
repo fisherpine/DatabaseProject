bc112a53ebd1acad60ab55ddcfa33e90d3923250
咕咕波仔 <94773509+fisherpine@users.noreply.github.com>
2023-08-15 17:06:00 +0800

    SDAO的基本增删改

--- src/main/java/com/ggbz/dao/SDAO.java
+++ src/main/java/com/ggbz/dao/SDAO.java
@@ -1,4 +1,25 @@
 package com.ggbz.dao;
 
+import com.ggbz.pojo.S;
+
+import java.util.List;
+
 public interface SDAO {
+    //学生表
+
+    /*添加学生*/
+    public void AddStudent(S student);
+
+    /*通过学号查询学生*/
+    public S QueryStudentById(String id);
+
+    /*查询所有学生*/
+    public List<S> QueryAllStudent();
+
+    /*通过学号删除学生*/
+    public void DeleteStudentById(String id);
+
+
+
+
 }
