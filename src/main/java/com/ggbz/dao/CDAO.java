639abcc0deec3f3d4501e38be06444707ad85bb2
咕咕波仔 <94773509+fisherpine@users.noreply.github.com>
2023-08-15 18:03:39 +0800

    CDAO部分功能实现

--- src/main/java/com/ggbz/dao/CDAO.java
+++ src/main/java/com/ggbz/dao/CDAO.java
@@ -1,4 +1,18 @@
 package com.ggbz.dao;
 
+import com.ggbz.pojo.C;
+
+import java.util.List;
+
 public interface CDAO {
+    /*课程表*/
+
+    /*查询所有课程*/
+    public List<C> QueryAllCourse();
+
+    /*通过课号查询课程*/
+    public C QueryCourseById(String id);
+
+    /*添加课程*/
+    public void AddCourse(C course);
 }
