ccc7497abdc12776808cedbcfcb7237f5b806258
咕咕波仔 <94773509+fisherpine@users.noreply.github.com>
2023-08-17 14:59:50 +0800

    修改学生信息功能

--- src/main/java/com/ggbz/service/StudentService.java
+++ src/main/java/com/ggbz/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.ggbz.service;
 
 import com.ggbz.pojo.Student;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 public interface StudentService {
+    public void UpdateStudentById(Student student);
 }
--- src/main/java/com/ggbz/service/impl/StudentSerivceImpl.java
+++ src/main/java/com/ggbz/service/impl/StudentSerivceImpl.java
@@ -1,6 +1,15 @@
 package com.ggbz.service.impl;
 
+import com.ggbz.dao.StudentDAO;
+import com.ggbz.pojo.Student;
 import com.ggbz.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class StudentSerivceImpl implements StudentService {
+    @Autowired
+    private StudentDAO studentDAO;
+    @Override
+    public void UpdateStudentById(Student student) {
+        studentDAO.UpdateStudentById(student);
+    }
 }
