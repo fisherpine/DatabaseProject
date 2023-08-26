bae513fd9597826c9fad9fc4a8158eb75cfe469c
咕咕波仔 <94773509+fisherpine@users.noreply.github.com>
2023-08-26 11:36:23 +0800

    绑定了点击事件，以及调整窗口位置

--- src/main/java/com/ggbz/view/StudentGradeManageSystem.java
+++ src/main/java/com/ggbz/view/StudentGradeManageSystem.java
@@ -1,15 +1,38 @@
 package com.ggbz.view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StudentGradeManageSystem extends JFrame {
     public void init(){
         JFrame frame = new JFrame("学生成绩管理系统");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
+        // 获取屏幕尺寸
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+        // 计算窗口位置
+        int centerX = (screenSize.width - frame.getWidth()) / 2;
+        int centerY = (screenSize.height - frame.getHeight()) / 2;
+
+        // 设置窗口位置
+        frame.setLocation(centerX, centerY);
         JMenuBar menuBar = new JMenuBar();
+
         JMenu studentMenu = new JMenu("学生信息管理");
+        JMenuItem studentjMenuItem = new JMenuItem("学生录入");
+        studentMenu.add(studentjMenuItem);
+        //给学生信息管理绑定点击事件
+        studentjMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new StudentMessageManage().init();
+                frame.setVisible(false);
+            }
+        });
+
         JMenu scoreMenu = new JMenu("学生成绩管理");
         JMenu systemMenu = new JMenu("系统管理");
 
