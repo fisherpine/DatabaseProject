8bf034198acc7c9eeace871e81831dbbe186b808
咕咕波仔 <94773509+fisherpine@users.noreply.github.com>
2023-08-26 12:08:55 +0800

    给Panel创建边框

--- src/main/java/com/ggbz/view/StudentMessageManage.java
+++ src/main/java/com/ggbz/view/StudentMessageManage.java
@@ -1,11 +1,14 @@
 package com.ggbz.view;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class StudentMessageManage extends JFrame {
     public void init(){
         JFrame frame = new JFrame("学生信息管理");
+        //frame布局
+        frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(600, 400);
         // 获取屏幕尺寸
@@ -17,6 +20,23 @@ public class StudentMessageManage extends JFrame {
 
         // 设置窗口位置
         frame.setLocation(centerX, centerY);
+
+
+
+        JPanel OperatejPanel = new JPanel();
+        OperatejPanel.setPreferredSize(new Dimension(500,300));
+
+        JPanel MessagejPanel = new JPanel();
+        MessagejPanel.setPreferredSize(new Dimension(500,100));
+
+        //创建边框
+        Border lineBorder = BorderFactory.createLineBorder(Color.GRAY, 1);
+        OperatejPanel.setBorder(lineBorder);
+        MessagejPanel.setBorder(lineBorder);
+
+        frame.add(OperatejPanel,BorderLayout.CENTER);
+        frame.add(MessagejPanel,BorderLayout.SOUTH);
+
         frame.setVisible(true);
     }
 
